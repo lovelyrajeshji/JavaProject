198023a773eeec5d4fbd6fe84ccec72133e9bb47
rajesh <kuvakollurajesh@gmail.com>
2025-08-04 17:19:40 +0530

    Added sysout statemnt

--- SecondLargestNumber.java
+++ SecondLargestNumber.java
@@ -5,6 +5,7 @@ public class SecondLargestNumber {
 	public static void main(String[] args) {
 
           		System.out.println("res1");
+                        System.out.println("res1");
 
 			}
 
