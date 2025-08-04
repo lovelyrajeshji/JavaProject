f197cb55b334a0828afa9d98b2c6efef78c2ebac
lovelyrajeshji <101453433+lovelyrajeshji@users.noreply.github.com>
2025-08-04 18:15:23 +0530

    Update SecondLargestNumber.java

--- SecondLargestNumber.java
+++ SecondLargestNumber.java
@@ -5,8 +5,6 @@ public class SecondLargestNumber {
 	public static void main(String[] args) {
 
           		System.out.println("res1");
-                        System.out.println("res1");
-		System.out.println("res1");
 
 			}
 
